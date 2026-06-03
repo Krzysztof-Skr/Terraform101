50efba7a8678aa9bc0e86965e63ffad1cef2b3f8
Krzysztof Skrajny <krzysztof.skrajny@bik.pl>
2026-06-03 13:47:05 +0200

    Configure Remote State in Lab3

---
+++ Lab3/versions.tf
@@ -0,0 +1,26 @@
+# Zdecydowanie zalecamy użycie bloku `required_providers` do określenia 
+# źródła i wersji dostawcy Azure, z którego korzystasz
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.74.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.9.0"
+    }
+  }
+  backend "azurerm" {
+    resource_group_name  = "rg-terraform-state-dev"
+    storage_account_name = "sthprtm8jqgd"
+    container_name       = "tfstate"
+    key                  = "obserability-dev"
+  }
+}
+
+# Skonfiguruj dostawcę Microsoft Azure
+provider "azurerm" {
+  features {}
+  subscription_id = "ec560136-63d2-4829-8eb1-c7fd3851b373"
+}
